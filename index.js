28b743bc0f94a454b29801947624cd36a76c6232
Aditya7848 <adityas7848@gmail.com>
2025-05-15 21:52:02 +0530

    writeFile -> appendFile -> rename

--- index.js
+++ index.js
@@ -13,23 +13,39 @@ fs.readFile(
   (err, data) => {
     if (err) throw err;
     // console.log(data.toString())
+    console.log('Read Complete.')
     console.log(data);
   }
 );
 
 console.log("Hello...");
 
-fs.writeFile(path.join(__dirname, "files", "reply.txt"),'bozo', (err) => {
+fs.writeFile(path.join(__dirname, "files", "reply.txt"),'Nice to meet you...', (err) => {
   if (err) throw err;
   // console.log(data.toString())
-  console.log("write Complete");
+  console.log("Write Complete.");
+
+  fs.appendFile(
+    path.join(__dirname, "files", "test.txt"),
+    "\n\n yes, yes you are asynced...",
+    (err) => {
+      if (err) throw err;
+      // console.log(data.toString())
+      console.log("Append Complete.");
+
+      fs.rename(
+        path.join(__dirname, "files", "reply.txt"),
+        path.join(__dirname, "files", "newReply.txt"),
+        
+        (err) => {
+          if (err) throw err;
+          // console.log(data.toString())
+          console.log("Rename Complete.");
+        });
+    });
 });
 
-fs.appendFile(path.join(__dirname, "files", "test.txt"),'testing...bozo', (err) => {
-  if (err) throw err;
-  // console.log(data.toString())
-  console.log("write Complete");
-});
+
 
 process.on("uncaughtException", (err) => {
   console.error(`There was an uncaught error : ${err}`);

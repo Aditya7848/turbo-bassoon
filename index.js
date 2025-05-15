30bd8656876a68fe08c78959dd22527683457cd5
Aditya7848 <adityas7848@gmail.com>
2025-05-15 22:19:31 +0530

    trying fsPromise -> you can write async and await code.

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require("path");
+
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "starter.txt"),
+      "utf8"
+    );
+    console.log("data = ",data);
+
+    await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"),data) //unlink is equal to delete...
+    await fsPromises.writeFile(path.join(__dirname, "files", "promiseWrite.txt"),data)
+    await fsPromises.appendFile(path.join(__dirname, "files", "promiseWrite.txt")," \n nice to meet you i just appended this file and fucked it up.")
+    await fsPromises.rename(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      path.join(__dirname, "files", "renamePromiseWrite.txt")
+    );
+
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "files", "renamePromiseWrite.txt"),
+      "utf8"
+    );
+    console.log("newData = ",newData);
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fileOps()
 
 // fs.readFile('./files/starter.txt','utf8', (err, data) => {
 //     if(err) throw err;
@@ -7,45 +36,41 @@ const path = require("path");
 //     console.log(data)
 // })
 
-fs.readFile(
-  path.join(__dirname, "files", "starter.txt"),
-  "utf8",
-  (err, data) => {
-    if (err) throw err;
-    // console.log(data.toString())
-    console.log('Read Complete.')
-    console.log(data);
-  }
-);
+// fs.readFile(
+//   path.join(__dirname, "files", "starter.txt"),
+//   "utf8",
+//   (err, data) => {
+//     if (err) throw err;
+//     // console.log(data.toString())
+//     console.log('Read Complete.')
+//     console.log(data);
+//   }
+// );
 
-console.log("Hello...");
+// fs.writeFile(path.join(__dirname, "files", "reply.txt"),'Nice to meet you...', (err) => {
+//   if (err) throw err;
+//   // console.log(data.toString())
+//   console.log("Write Complete.");
 
-fs.writeFile(path.join(__dirname, "files", "reply.txt"),'Nice to meet you...', (err) => {
-  if (err) throw err;
-  // console.log(data.toString())
-  console.log("Write Complete.");
+//   fs.appendFile(
+//     path.join(__dirname, "files", "test.txt"),
+//     "\n\n yes, yes you are asynced...",
+//     (err) => {
+//       if (err) throw err;
+//       // console.log(data.toString())
+//       console.log("Append Complete.");
 
-  fs.appendFile(
-    path.join(__dirname, "files", "test.txt"),
-    "\n\n yes, yes you are asynced...",
-    (err) => {
-      if (err) throw err;
-      // console.log(data.toString())
-      console.log("Append Complete.");
+//       fs.rename(
+//         path.join(__dirname, "files", "reply.txt"),
+//         path.join(__dirname, "files", "newReply.txt"),
 
-      fs.rename(
-        path.join(__dirname, "files", "reply.txt"),
-        path.join(__dirname, "files", "newReply.txt"),
-        
-        (err) => {
-          if (err) throw err;
-          // console.log(data.toString())
-          console.log("Rename Complete.");
-        });
-    });
-});
-
-
+//         (err) => {
+//           if (err) throw err;
+//           // console.log(data.toString())
+//           console.log("Rename Complete.");
+//         });
+//     });
+// });
 
 process.on("uncaughtException", (err) => {
   console.error(`There was an uncaught error : ${err}`);

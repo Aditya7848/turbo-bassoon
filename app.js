2d7f0fe16d961a80aa6b5732937371389e29c213
Aditya7848 <adityas7848@gmail.com>
2025-05-15 20:21:14 +0530

    worked with os, path, custom path

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('to n')
+// CommonJS (require()) and ES6 modules (import/export).
+
+// Exports	Single object (module.exports)	Named exports and default exports
+
+// Loading	Synchronous	Asynchronous

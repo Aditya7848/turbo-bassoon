203d7998078d24b71e0d4f8dd0fc7925e14bd12c
Aditya7848 <adityas7848@gmail.com>
2025-05-15 21:40:04 +0530

    doing fs operations

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
 
-fs.readFile('./files/starter.txt','utf8', (err, data) => {
-    if(err) throw err;
+// fs.readFile('./files/starter.txt','utf8', (err, data) => {
+//     if(err) throw err;
+//     // console.log(data.toString())
+//     console.log(data)
+// })
+
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf8",
+  (err, data) => {
+    if (err) throw err;
     // console.log(data.toString())
-    console.log(data)
-})
+    console.log(data);
+  }
+);
+
+console.log("Hello...");
+
+fs.writeFile(path.join(__dirname, "files", "reply.txt"),'bozo', (err) => {
+  if (err) throw err;
+  // console.log(data.toString())
+  console.log("write Complete");
+});
+
+fs.appendFile(path.join(__dirname, "files", "test.txt"),'testing...bozo', (err) => {
+  if (err) throw err;
+  // console.log(data.toString())
+  console.log("write Complete");
+});
+
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncaught error : ${err}`);
+  process.exit(1);
+});
